47a0f2943b56344fe57d2eb65c4a15df31780707
xuexiji <xuexiji@123.cn>
2024-07-13 21:12:19 +0800

    first commit

---
+++ src/main/java/com/xw/xwojcodesandbox/XwojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.xw.xwojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XwojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XwojCodeSandboxApplication.class, args);
+    }
+
+}
