9f0910d235854023fa3a435dd67b24b4bb541981
xuexiji <xuexiji@123.cn>
2024-07-24 03:55:51 +0800

    first commit

--- src/main/java/com/xw/xwojcodesandbox/model/ExecuteMessage.java
+++ src/main/java/com/xw/xwojcodesandbox/model/ExecuteMessage.java
@@ -15,4 +15,7 @@ public class ExecuteMessage {
     private String errorMessage;
 
     private Long time;
+
+    private Long memory;
+
 }
---
+++ src/main/java/com/xw/xwojcodesandbox/testLeetCode.java
@@ -0,0 +1,23 @@
+//package com.xw.xwojcodesandbox;
+//
+//import javax.annotation.Resource;
+//
+//public class testLeetCode {
+//
+//
+//    public static void main(String[] args) {
+//        testLeetCode qwe = new testLeetCode();
+//        int nums[] ={1,1,2};
+//        int k = qwe.removeDuplicates(nums);
+//        System.out.println(k);
+//    }
+//
+//        public int removeDuplicates(int[] nums) {
+//        int a = 0;
+//        int b = nums.length;
+//        if (b == 0) {
+//            return 0;
+//        }
+//        return k;
+//    }
+//}
